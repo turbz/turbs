c4f858ea09579f0ea179eccabeb74dd1f55db4fc
thabisoddx <thabisomoha@gmail.com>
2021-05-19 12:21:09 +0200

    Updates

---
+++ component---src-pages-404-js-cfa2a2b78f8b421fdf37.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[883],{9616:function(e,t,l){"use strict";l.r(t);var r=l(7294);t.default=function(){return r.createElement("div",null,r.createElement("h1",null,"Page Not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-cfa2a2b78f8b421fdf37.js.map
---
+++ component---src-pages-index-js-d2913590acd1eb151850.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{7704:function(e,t,l){"use strict";l.r(t),l.d(t,{default:function(){return n}});var r=l(7294);function n(){return r.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-d2913590acd1eb151850.js.map
